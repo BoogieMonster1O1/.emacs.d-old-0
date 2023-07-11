40065864cd2d08b15600a1c9adf3d98a2842bfbd
Shrish Deshpande <shrish.deshpande@icloud.com>
2023-07-11 09:49:32 +0530

    emacs config

---
+++ init.el
@@ -0,0 +1,69 @@
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
+(use-package treemacs
+  :ensure t
+  :defer t
+  :config
+  (progn
+    (setq treemacs-follow-after-init t
+          treemacs-width 35
+          treemacs-indentation 2
+          treemacs-collapse-dirs 3
+          treemacs-silent-refresh t
+          treemacs-change-root-without-asking t
+          treemacs-sorting 'alphabetic-desc
+          treemacs-show-hidden-files t
+          treemacs-never-persist nil
+          treemacs-is-never-other-window nil
+          treemacs-goto-tag-strategy 'refetch-index)
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (pcase (cons (not (null (executable-find "git")))
+                 (not (null (executable-find "python3"))))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+        ("M-0"       . treemacs-select-window)
+        ("C-x t 1"   . treemacs-delete-other-windows)
+        ("C-x t t"   . treemacs)
+        ("C-x t B"   . treemacs-bookmark)
+        ("C-x t C-t" . treemacs-find-file)
+        ("C-x t M-t" . treemacs-find-tag)))
+
+(require 'gradle-mode)
+(add-hook 'java-mode-hook '(lambda() (gradle-mode 1)))
+
+(use-package company)
+
+(use-package lsp-ui
+  :after lsp-mode
+  :hook (lsp-mode . lsp-ui-mode)
+  :config
+  (setq lsp-ui-sideline-enable t
+        lsp-ui-sideline-show-diagnostics t
+        lsp-ui-sideline-show-hover t
+        lsp-ui-sideline-show-code-actions t
+        lsp-ui-sideline-update-mode 'line))
+
+(add-to-list 'auto-mode-alist '("\\.tsx?$" . web-mode))
+(lsp-treemacs-sync-mode 1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(company lsp-ui lsp-java gradle-mode lsp-treemacs flycheck web-mode treemacs dired-sidebar lsp-mode typescript-mode ## clojure-mode csharp-mode go-mode javaimp json-mode kotlin-mode windresize 
+yaml-mode)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
